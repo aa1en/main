46edc7fae2afce57e7f29f51bcf14cb7fb19147b
aa1en <aa1en@mail.ru>
2022-06-30 23:52:22 +0400

    First Versin

--- lesson.py
+++ lesson.py
@@ -1,1 +1,18 @@
-print('Hello world')
+my_list = []
+while True:
+    num = input('Enter number: ')
+    if num == '':
+        break
+    else:
+        my_list.append(int(num))
+print(my_list)
+finish = int(input('Enter finish number: '))
+for i in my_list:
+    for j in my_list:
+        if i + j == finish:
+            print(f'{finish} = ({my_list.index(i)},{my_list.index(j)})')
+            break
+        else:
+            continue
+    break
+# Finish
