ef87fa04f65e8214cf0c84e43a755a8c48719ae6
aa1en <aa1en@mail.ru>
2022-07-01 14:19:36 +0400

    test

--- lesson.py
+++ lesson.py
@@ -1,18 +1,37 @@
-my_list = []
-while True:
-    num = input('Enter number: ')
-    if num == '':
-        break
-    else:
-        my_list.append(int(num))
-print(my_list)
-finish = int(input('Enter finish number: '))
-for i in my_list:
-    for j in my_list:
-        if i + j == finish:
-            print(f'{finish} = ({my_list.index(i)},{my_list.index(j)})')
-            break
-        else:
-            continue
-    break
-# Finish
+# my_list = []
+# while True:
+#     num = input('Enter number: ')
+#     if num == '':
+#         break
+#     else:
+#         my_list.append(int(num))
+# print(my_list)
+# finish = int(input('Enter finish number: '))
+# for i in my_list:
+#     for j in my_list:
+#         if i + j == finish:
+#             print(f'{finish} = ({my_list.index(i)},{my_list.index(j)})')
+#             break
+#         else:
+#             continue
+#     break
+# # Finish
+
+mylist = [1, 2, 3, 4, 5]
+list = []
+for i in range(0, len(mylist) - 1):
+  
+
+
+    # for j in range(0, len(mylist) - 1):
+    #     mylist[j],mylist[j+1] = mylist[j+1],mylist[j]
+    #     print(mylist)
+    #     for m in range(0, len(mylist) - 1):
+    #         mylist[m],mylist[m+1] = mylist[m+1],mylist[m]
+    #         print(mylist)
+            # for n in range(0, len(mylist) - 1):
+            #     mylist[n],mylist[n+1] = mylist[n+1],mylist[n]
+            #     print(mylist)
+            #     for l in range(0, len(mylist) - 1):
+            #         mylist[l],mylist[l+1] = mylist[l+1],mylist[l]
+            #         print(mylist)
