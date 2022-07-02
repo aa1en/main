3ed5e54d90f73ffebdca5dff07b8dcd77a657f08
aa1en <aa1en@mail.ru>
2022-07-03 00:55:34 +0400

    Functions

--- lesson.py
+++ lesson.py
@@ -1,37 +1,121 @@
+"""1.CALCULATOR"""
+# def add(x, y):
+#     return x + y
+
+
+# def subtract(x, y):
+#     return x - y
+
+
+# def multiply(x, y):
+#     return x * y
+
+
+# def divide(x, y):
+#     return x / y
+
+
+# print("Select operation. \n1)Add \n2)Subtract \n3)Multiply \n4)Divide")
+# while True:
+#     choice = input("Enter choice(1,2,3,4): ")
+#     if choice in ('1', '2', '3', '4'):
+#         num1 = float(input("Enter first number: "))
+#         num2 = float(input("Enter second number: "))
+
+#         if choice == '1':
+#             print(f'{num1} + {num2} = {add(num1, num2)}')
+#         elif choice == '2':
+#             print(f'{num1} - {num2} = {subtract(num1, num2)}')
+#         elif choice == '3':
+#             print(f'{num1} * {num2} = {multiply(num1, num2)}')
+#         elif choice == '4':
+#             print(f'{num1} / {num2} = {divide(num1, num2)}')
+        
+
+#         next_calculation = input("Let's do next calculation? (yes/no): ")
+#         if next_calculation == "no":
+#           break
+    
+#     else:
+#         print("Invalid Input")
+
+"""2.MAX NUMBER"""
+# def max_number(x,y):
+#     if x - y > 0:
+#         return  f'{x} is max'
+#     elif x - y == 0:
+#         return 'Numbers are equal'
+#     else:
+#         return f'{y} is max'
+
+
+# num1 = float(input('Enter number 1: '))
+# num2 = float(input('Enter number 2: '))
+# print(max_number(num1, num2))
+
+"""3.SUM ALL NUMBERS"""
+
+# def sum_all_numbers(x):
+#     return sum(x)
+
+
+# numbers_quantity = int(input('How many numbers you want to enter: '))
 # my_list = []
 # while True:
-#     num = input('Enter number: ')
-#     if num == '':
+#     if len(my_list) == numbers_quantity:
 #         break
 #     else:
-#         my_list.append(int(num))
-# print(my_list)
-# finish = int(input('Enter finish number: '))
-# for i in my_list:
-#     for j in my_list:
-#         if i + j == finish:
-#             print(f'{finish} = ({my_list.index(i)},{my_list.index(j)})')
-#             break
+#         number = float(input('Enter number for sum: '))
+#         my_list.append(number)
+# print(sum_all_numbers(my_list))
+
+"""4.MULTIPLY ALL NUMBERS"""
+
+# def multiply_nums(x):
+#     result = 1
+#     for num in x:
+#         result *= num
+#     return result
+
+
+# my_list = []
+# number = int(input('How many numbers you want to multiply? >'))
+# while True:
+#     if len(my_list) == number:
+#         break
+#     else:
+#         num_multy = int(input('Enter number for multiply: '))
+#         my_list.append(num_multy)
+# print(multiply_nums(my_list))
+
+"""5.LETTER AND NUMBER"""
+
+# def letter_nums_sum(str):
+#     letters = 'abcdefghijklmnopqrstuvwxyz'
+#     sum = 0
+#     for i in str:
+#         if i in letters:
+#             sum += (letters.index(i) + 1)
 #         else:
-#             continue
-#     break
-# # Finish
-
-mylist = [1, 2, 3, 4, 5]
-list = []
-for i in range(0, len(mylist) - 1):
-  
+#             sum += int(i)
+#     return sum
 
 
-    # for j in range(0, len(mylist) - 1):
-    #     mylist[j],mylist[j+1] = mylist[j+1],mylist[j]
-    #     print(mylist)
-    #     for m in range(0, len(mylist) - 1):
-    #         mylist[m],mylist[m+1] = mylist[m+1],mylist[m]
-    #         print(mylist)
-            # for n in range(0, len(mylist) - 1):
-            #     mylist[n],mylist[n+1] = mylist[n+1],mylist[n]
-            #     print(mylist)
-            #     for l in range(0, len(mylist) - 1):
-            #         mylist[l],mylist[l+1] = mylist[l+1],mylist[l]
-            #         print(mylist)
+# string =input('Enter text: ').lower()
+# print(letter_nums_sum(string))
+
+"""6.GET READY!"""
+
+# def get_ready(x,y,z):
+#     if x > 16 and y > 16 and z > 16:
+#         return "Get ready!"
+#     else:
+#         return "Too young!"
+
+
+# x = int(input('Enter your age: '))
+# y = int(input('Enter your age: '))
+# z = int(input('Enter your age: '))
+# print(get_ready(x, y, z))
+
+
